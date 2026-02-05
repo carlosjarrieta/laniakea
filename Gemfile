073b21f4581c740ec9ef72c1a8767bff37dd0a8e
Carlos Arrieta <carlosj.arrieta@gmail.com>
2026-02-05 10:45:51 -0500

    Commit initial

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source "https://rubygems.org"
+
+ruby "3.2.2"
+gem "rails", "~> 7.1.6"
+gem "pg", "~> 1.1"
+gem "puma", ">= 5.0"
+gem "tzinfo-data", platforms: %i[ mswin mswin64 mingw x64_mingw jruby ]
+gem "bootsnap", require: false
+# gem "image_processing", "~> 1.2"
+# gem "rack-cors"
+# gem "rack-cors"
+
+group :development, :test do
+end
+
+group :development do
+end
+
+
+gem "figaro", "~> 1.3"
+
+gem "devise", "~> 5.0"
+gem "devise-jwt", "~> 0.13.0"
+gem "rack-cors", "~> 3.0"
---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :trackable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
+  enum role: { advertiser: 0, superadmin: 1 }
+
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role ||= :advertiser
+  end
+end
